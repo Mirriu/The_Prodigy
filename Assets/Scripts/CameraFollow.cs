c3db39c46ba00faccd71893fd4f35afe4a1bd58b
grommithub <44238640+grommithub@users.noreply.github.com>
2022-03-15 13:14:49 +0100

    lol

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform followObject;
+    public Rigidbody2D followRb;
+    private Vector3 intendedPosition;
+
+    private void Start()
+    {
+        followObject = FindObjectOfType<PlayerController>()?.transform;
+        followRb = followObject?.GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        intendedPosition = followObject.position;
+        intendedPosition += (Vector3)followRb?.velocity / 15.0f;
+
+
+        transform.position = intendedPosition;
+
+        transform.position = new Vector3(transform.position.x, transform.position.y, -10f);
+    }
+
+
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,12 +7,21 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb;
     public float[] thrusters = new float[2];
-    [SerializeField] private float turningSpeed = 5.0f, acceleration = 10.0f;
+    [SerializeField] private float turningSpeed = 5.0f, acceleration = 10.0f, wallBoostForward = 3.0f, wallBoostOutward = 3.0f;
     public bool inverted = true;
 
+    public bool boostStraight = false;
+
     public LineRenderer[] thrusterLines;
+    public Transform[] boostParticles;
     public float thrusterLineLength = 5.0f;
 
+    public float wallPushForce = 3.0f, wallPushDistance = 7.0f;
+
+    private float defaultDrag, extraDrag;
+
+    private AudioSource audioSource;
+    private float vol;
 
     Gamepad controls;
     Keyboard keyboard;
@@ -27,6 +36,21 @@ public class PlayerController : MonoBehaviour
         {
             keyboard = Keyboard.current;
         }
+
+        defaultDrag = rb.drag;
+        extraDrag = defaultDrag * wallPushForce;
+        audioSource = GetComponent<AudioSource>();
+        vol = audioSource.volume;
+    }
+
+    private void Update()
+    {
+        
+        thrusterLines[0]?.SetPosition(0, thrusterLines[0].transform.position);
+        thrusterLines[1]?.SetPosition(0, thrusterLines[1].transform.position);
+
+        thrusterLines[0]?.SetPosition(1, thrusterLines[0].transform.position - thrusterLines[0].transform.up * thrusters[0] * thrusterLineLength);
+        thrusterLines[1]?.SetPosition(1, thrusterLines[1].transform.position - thrusterLines[1].transform.up * thrusters[1] * thrusterLineLength);
     }
 
     private void FixedUpdate()
@@ -34,40 +58,67 @@ public class PlayerController : MonoBehaviour
         SetThrusterStrengths();
         transform.rotation *= Quaternion.Euler(0f, 0f, (thrusters[1] - thrusters[0]) * turningSpeed);
         rb.AddForce(transform.up * (thrusters[0] + thrusters[1]) * acceleration);
+        audioSource.volume = 0.0f;
+        AddWallBoost();
+        WallSlowDown();
     }
 
     private void SetThrusterStrengths()
     {
-        if(usingGamepad && false)
+        if(usingGamepad)
         {
             thrusters[inverted ? 1 : 0] = controls.leftTrigger.ReadValue();
             thrusters[inverted ? 0 : 1] = controls.rightTrigger.ReadValue();
         }
         else
         {
-            thrusters[inverted ? 1 : 0] = Mathf.Lerp(thrusters[inverted ? 1 : 0], (keyboard.eKey.isPressed ? 1f : 0f), 0.06f);
-            thrusters[inverted ? 0 : 1] = Mathf.Lerp(thrusters[inverted ? 0 : 1], (keyboard.iKey.isPressed ? 1f : 0f), 0.06f);
+            thrusters[inverted ? 1 : 0] = Mathf.Lerp(thrusters[inverted ? 1 : 0], (Keyboard.current.eKey.isPressed ? 1f : 0f), 0.06f);
+            thrusters[inverted ? 0 : 1] = Mathf.Lerp(thrusters[inverted ? 0 : 1], (Keyboard.current.iKey.isPressed ? 1f : 0f), 0.06f);
         }
 
+    }
 
-
-        thrusterLines[0]?.SetPosition(0, thrusterLines[0].transform.position);
-        thrusterLines[1]?.SetPosition(0, thrusterLines[1].transform.position);
-
-        thrusterLines[0]?.SetPosition(1, thrusterLines[0].transform.position - transform.up * thrusters[0] * thrusterLineLength);
-        thrusterLines[1]?.SetPosition(1, thrusterLines[1].transform.position - transform.up * thrusters[1] * thrusterLineLength);
-
+    float GetThrusterLineLength(int index)
+    {
+        return (thrusterLines[index].GetPosition(1) - thrusterLines[index].GetPosition(1)).magnitude;
     }
 
     void AddWallBoost()
     {
         for (int i = 0; i < thrusters.Length; i++)
         {
-            var hit = Physics2D.Raycast(thrusterLines[i].transform.position, -thrusterLines[i].transform.up, thrusters[i]);
+            var hit = Physics2D.Raycast(thrusterLines[i].transform.position, -thrusterLines[i].transform.up, thrusters[i] * thrusterLineLength);
             if(hit)
             {
-                rb.AddForce(transform.up * acceleration * thrusters[i] * 1000.0f);
+                rb.AddForce(thrusterLines[i].transform.up * acceleration * thrusters[i] * wallBoostOutward);
+                rb.AddForce(transform.up * acceleration * thrusters[i] * wallBoostForward);
+                boostParticles[i].transform.position = hit.point;
+                boostParticles[i].transform.right = -transform.up;
+                audioSource.volume = vol;
             }
+            else
+            {
+                boostParticles[i].transform.position = new Vector3(0f, 0f, -10000f);
+            }
+
         }
     }
+
+
+
+    void WallSlowDown()
+    {
+        var hit = Physics2D.Raycast(transform.position + transform.right * wallPushDistance, -transform.right, 2f * wallPushDistance);
+       
+        if(hit)
+        {
+            var direction = -(hit.point - (Vector2)transform.position).normalized;
+            rb.AddForce(direction * wallPushForce);
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawRay(transform.position + transform.right * wallPushDistance, -transform.right * wallPushDistance * 2f);
+    }
+
 }
